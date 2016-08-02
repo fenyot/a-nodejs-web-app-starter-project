66d835607a565806031d452d697e3619243d0782
Tamas Fenyofalvi <fenyot@gmail.com>
2016-08-02 15:47:10 +0200

    Add admin server launcher

--- admin/app.js
+++ admin/app.js
@@ -1,1 +1,30 @@
 'use strict'
+
+const path = require('path')
+const glob = require('glob')
+const http = require('http')
+const app = require('koa')()
+const bodyParser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const conf = require('./config/config')()
+
+let server
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(logger())
+}
+
+app.use(bodyParser())
+
+glob.sync(path.join(__dirname, 'route', '**', '*.js')).forEach(file => {
+  app.use(require(file).routes())
+})
+
+const httpServer = http.createServer(app.callback())
+module.exports = httpServer
+
+httpServer.listen(conf.port, err => {
+  if (err) throw err
+  server = httpServer
+  console.log(`Admin ${conf.mode} server is listening on port ${server.address().port}`)
+})
